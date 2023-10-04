9a9aa282edd1527d45e5dafbd16f75f5414caad9
DanielRece <recedm98@gmail.com>
2023-10-04 09:12:50 +0200

    eliminadas carpetas build y P2 v2

--- ANIOTP2/timers/main/main.c
+++ ANIOTP2/timers/main/main.c
@@ -13,7 +13,7 @@
 
 //static const char *TAG = "main";
 
-static int BLINK_GPIOS[4] = {5,18,21,22};
+static gpio_num_t led_gpios[4] = {GPIO_NUM_12,GPIO_NUM_13,GPIO_NUM_14,GPIO_NUM_15};
 static uint8_t s_led_states[4] = {0,0,0,0};
 
 static float temperature;
@@ -26,38 +26,24 @@ static void read_temperature_timer_callback(void* arg){
         while (cp_temp > 0 && i < 4){
                 s_led_states[i] = 1;
                 cp_temp -=2;
+                gpio_set_level(led_gpios[i], s_led_states[i]);
                 i++;
         }
         while (i < 4){
                 s_led_states[i]=0;
+                gpio_set_level(led_gpios[i], s_led_states[i]);
                 i++;
         }
-        gpio_set_level(BLINK_GPIOS[0], s_led_states[0]);
-        gpio_set_level(BLINK_GPIOS[1], s_led_states[1]);
-        gpio_set_level(BLINK_GPIOS[2], s_led_states[2]);
-        gpio_set_level(BLINK_GPIOS[3], s_led_states[3]);
-        
-        
-        // TODO: Encender/apagar LEDs en función de la temperatura
-	/*
-		EJEMPLO DE PSEUDOCODIGO DE CÓMO HACER LA LÓGICA
-  (HABRÍA QUE INICIALIZAR LOS GPIOS EN EL MAIN)
-  temp -= 20;
-i = 0;
-while (temp > 0 && i < 4){
- encender_led(gpio[i]);
- temp -= 2;
- i++;
-}
-// APAGAR LOS LEDS RESTANTES
-while(i < 4){
-apagar_led(gpio[i]);
-i++;
 }
 
-	*/
-
+static void configure_leds(void)
+{
+	for (int i = 0; i < 4; i++){
+		gpio_reset_pin(led_gpios[i]);
+		gpio_set_direction(led_gpios[i], GPIO_MODE_OUTPUT);
+	}
 }
+
 static void print_temperature_timer_callback(void* arg){
 	ESP_LOGI("PRINT_TIMER","Temperature: %f", temperature);
 }
@@ -82,13 +68,12 @@ void app_main(void)
 
         i2c_master_init();
 
+        configure_leds();
 
         ESP_ERROR_CHECK(esp_timer_create(&read_timer_args, &read_timer));
-
         ESP_ERROR_CHECK(esp_timer_create(&print_timer_args, &print_timer));
         
         ESP_ERROR_CHECK(esp_timer_start_periodic(read_timer, 1000000));
-
         ESP_ERROR_CHECK(esp_timer_start_periodic(print_timer, 10000000));
 }
 
